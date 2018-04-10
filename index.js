e18c3a4ecede04cff5105e9457daa7191e5802dc
Zhang Lihua <zhang.lihua@wanglutech.com>
2018-04-10 17:54:56 +0800

    add promisefyEvent

--- index.js
+++ index.js
@@ -21,5 +21,20 @@ function promisefy(func, paras=[], obj=null){
         func.apply(obj, paras);
     });
 }
+function promiseEvent(func, paras=[], obj=null, event){
+    return new Promise(function(success, fail){
+        let res = func.apply(obj, paras);
+        obj.on(event, function _cb(err){
+            if(err){
+                fail(err);
+            } else {
+                success(res);
+            }
+        })
+    });
+}
+
+exports.promiseEvent = promiseEvent;
 exports.promisefy = promisefy;
+
 exports.sleep = sleep;
